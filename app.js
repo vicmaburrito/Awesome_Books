b659f7cc0c9876dcfa98632bc34fe8e11caeaa27
AngelMoraDev <angelmomaa@gmail.com>
2021-09-14 11:18:23 -0500

    playing around with standup to get started

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('holamundo');
+
+const inputTitle = document.querySelectorAll('#title');
+const inputAuthor = document.querySelectorAll('#author');
+
+document.createElement('tbody')
+// append element
+// create a function to add a book
+const books = []
+
+function addBook(title, author) {
+  inputTitle.value
+};
+
+addBook(title, author);
