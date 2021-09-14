05988639a3acacd8213a79040e48033f15213a7e
AngelMoraDev <angelmomaa@gmail.com>
2021-09-14 17:47:44 -0500

    start functions structure

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const inputTitle = document.querySelectorAll('#title');
 const inputAuthor = document.querySelectorAll('#author');
-
-// append element
-// create a function to add a book
 const books = []
+
+// Local Storage
+
+function allBooks() {
+  // get books from local storage
+}
+
+
+// User Interface
 
 function addBook() {
   const createRow = document.getElementsByClassName("table-body").createElement("tr");
   createRow.appendChild("td", title, body);
 };
 
-addBook();
+
