9a471105535da47446b1a92abebd2770d25fb025
AngelMoraDev <angelmomaa@gmail.com>
2021-09-14 19:53:03 -0500

    set basic structure for three parts of UI

--- app.js
+++ app.js
@@ -16,4 +16,32 @@ function addBook() {
   createRow.appendChild("td", title, body);
 };
 
+function displayBooks() {
+  // Show all books in table
+}
 
+function removeBook(book) {
+  // Remove book from DOM
+}
+
+// Events handling
+
+// DOM to display books when content loaded
+
+document.querySelector('#form').addEventListener('submit', (t) => {
+  // default action
+  t.preventDefault();
+
+  // title, author values
+
+  // Create book in storage
+
+  // Create book in UI
+
+});
+
+document.querySelector('#form').addEventListener('click', (t) => {
+  removeBook(t.target);
+
+  // erase target title and author parent tags
+});
